24aa3a4f94a41e08a09410531bce7075dcb84513
BrianU <brian.ulatowski@gmail.com>
2020-01-15 22:03:12 -0800

    user input questions and var for all selected types

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
 //declare character variables
-var lowerCase = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-var upperCase = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-var numList = array('0','1','2','3','4','5','6','7','8','9');
-var specChar = array('!','@','#','$','?','+');
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numList = "0123456789";
+var specChar = "!@#$?+";
 
 //Alerts to inform user of password generation and lowercase inclusion
 alert("Hello! Let's make a password!");
 
 //confirm statements to determine user choice for each
-var  = prompt();
+var passLength = Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));
 
-function input() {
-    var passLength = Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));
-    if (isNaN(passLength)); //Check what number it is
+if (passLength < 8 || passLength > 128 || isNaN(passLength)); {//Check if response is a number and within required range
     alert("Not Valid. Try again.");
-    return input();
-    }
- 
-//compare converted response to 8 and 128, if < 8 or > 128, then alert user it's not valid and must be >= 8 or <= 128 
+    Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));    
+} //want to loop back through if statement until user input meets conditional   
 
-} else (alert("Lowercase letters will be automatically included."));
+//else {
 
-function generatePass(passLength) {
-
-var useSpec = confirm("Would you like to include special characters?");
-if (useSpec == true) {
-    
-} else specChar = null;
- 
-var useNum = confirm("Would you like to include numeric characters?");
-if (numChar == true) {
-    
-} else numList = null;
-
-var uppChar = confirm("Would you like to include uppercase characters?");
-if (uppChar == true) {
-    
-} else upperCase = null;
-
-//variable to include all the selected types
-var include = specChar + numChar + uppChar + lowerCase;
-
-
-var pass = ""; 
-
-//for (var i = 0; i < passLength; i++) {
-//     var character = Math.floor(Math.random() * include.length);
-//     pass += all.substring(character, character + 1);
+alert("Lowercase letters will be automatically included.");
 //}
 
-//return pass;
+//getting user inout to include special characters or not
+var useSpec = confirm("Would you like to include special characters?");
+if (useSpec !== true); {
+    specChar = null;    
+} 
+
+//getting user inout to include numeric characters or not 
+var useNum = confirm("Would you like to include numeric characters?");
+if (useNum !== true) {
+    numList = null;
+}  
+
+//getting user inout to include uppercase characters or not
+var uppChar = confirm("Would you like to include uppercase characters?");
+if (uppChar = true) {
+    upperCase = null;  
+} 
+
+//variable to include all the selected types
+var incChar = lowerCase + specChar + numList + upperCase;
+
+//function to generate password
+function generatePass(passLength) {
+    for (var i = 0; i < passLength; i++) {
+    passWord = passLength[Math.floor(Math.random() * passLength.length)];
+}
+
+//return the password
+return passWord;
 
 }
 
