98e2fda13fe7b294e223987fb411cb6720b57902
BrianU <brian.ulatowski@gmail.com>
2020-01-17 20:29:51 -0800

    random number generator added

--- script.js
+++ script.js
@@ -3,53 +3,67 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numList = "0123456789";
 var specChar = "!@#$?+";
-
+var passWord = "";
 //Alerts to inform user of password generation and lowercase inclusion
 alert("Hello! Let's make a password!");
 
 //confirm statements to determine user choice for each
 var passLength = Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));
 
-if (passLength < 8 || passLength > 128 || isNaN(passLength)); {//Check if response is a number and within required range
-    alert("Not Valid. Try again.");
-    Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));    
-} //want to loop back through if statement until user input meets conditional   
+//function ; {
+//passLength = getPasswordLength()
+    if ((passLength < 8) || (passLength > 128) || (isNaN(passLength))) {//Check if response is a number and within required range
+        alert("Not Valid. Try again.");
+        Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));    
+
+    } //want to loop back through if statement until user input meets conditional   
+//}  return passLength;
 
 //else {
 
 alert("Lowercase letters will be automatically included.");
 //}
 
-//getting user inout to include special characters or not
+//getting user input to include special characters or not
 var useSpec = confirm("Would you like to include special characters?");
-if (useSpec !== true); {
+if (!useSpec) {
     specChar = null;    
 } 
 
-//getting user inout to include numeric characters or not 
+//getting user input to include numeric characters or not 
 var useNum = confirm("Would you like to include numeric characters?");
-if (useNum !== true) {
+if (!useNum) {
     numList = null;
 }  
 
-//getting user inout to include uppercase characters or not
+//getting user input to include uppercase characters or not
 var uppChar = confirm("Would you like to include uppercase characters?");
-if (uppChar = true) {
+if (!uppChar) {
     upperCase = null;  
 } 
 
 //variable to include all the selected types
 var incChar = lowerCase + specChar + numList + upperCase;
 
+console.log(incChar);
 //function to generate password
 function generatePass(passLength) {
     for (var i = 0; i < passLength; i++) {
-    passWord = passLength[Math.floor(Math.random() * passLength.length)];
+        console.log(Math.ceil(Math.random() * passLength))
+
+    // passWord = Math.floor(Math.random() * passLength.length);
+        //+=
+        //stringArray
+        // step 1 - generate random number to represent index in "inChar" string
+        // step 2 - add the character at that index to the end of the passWord string
+    }
+    
+    //return the password
+    return passWord;
 }
 
-//return the password
-return passWord;
+//invoke function
 
-}
+alert(generatePass(passLength));
 
 console.log("work");
