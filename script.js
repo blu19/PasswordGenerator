1bdfae1a84ac68cd1f51f1b12c422d02d2059ca7
BrianU <brian.ulatowski@gmail.com>
2020-01-18 21:11:02 -0800

    added prettier app; moved variables inside function; implemented init function from the start; made unwanted character variables null within function generatePass; added alert with given password.

--- script.js
+++ script.js
@@ -1,69 +1,77 @@
-//declare character variables
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numList = "0123456789";
-var specChar = "!@#$?+";
-var passWord = "";
-//Alerts to inform user of password generation and lowercase inclusion
-alert("Hello! Let's make a password!");
+function init() {
 
-//confirm statements to determine user choice for each
-var passLength = Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));
+    //Alerts to inform user of password generation and lowercase inclusion
+    alert("Hello! Let's make a password!");
 
-//function ; {
-//passLength = getPasswordLength()
-    if ((passLength < 8) || (passLength > 128) || (isNaN(passLength))) {//Check if response is a number and within required range
-        alert("Not Valid. Try again.");
-        Number(prompt("How many characters do you want your password to be? (Min 8 and max 128)"));    
+    //confirm statements to determine user choice for each
+    var passLength = Number(
+    prompt(
+        "How many characters do you want your password to be? (Min 8 and max 128)"
+    )
+    );
 
-    } //want to loop back through if statement until user input meets conditional   
-//}  return passLength;
+    //if statement to make sure user passwrodlength meets conditionals
+    if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
 
-//else {
+    //Check if response is a number and within required range
+    alert("Not Valid. Try again.");
+    Number(
+        prompt(
+        "How many characters do you want your password to be? (Min 8 and max 128)"
+        )
+    );
+    } //want to loop back through if statement until user input meets conditional
 
-alert("Lowercase letters will be automatically included.");
-//}
+    //alert to notify user password will automatically include lowercase characters
+    alert("Lowercase letters will be automatically included.");
 
-//getting user input to include special characters or not
-var useSpec = confirm("Would you like to include special characters?");
-if (!useSpec) {
-    specChar = null;    
-} 
+    //getting user input to include special characters or not
+    var useSpec = confirm(
+    "Would you like to include special characters?"
+    );
 
-//getting user input to include numeric characters or not 
-var useNum = confirm("Would you like to include numeric characters?");
-if (!useNum) {
-    numList = null;
-}  
+    //getting user input to include numeric characters or not
+    var useNum = confirm(
+    "Would you like to include numeric characters?"
+    );
 
-//getting user input to include uppercase characters or not
-var uppChar = confirm("Would you like to include uppercase characters?");
-if (!uppChar) {
-    upperCase = null;  
-} 
+    //getting user input to include uppercase characters or not
+    var uppChar = confirm(
+    "Would you like to include uppercase characters?"
+    );
 
-//variable to include all the selected types
-var incChar = lowerCase + specChar + numList + upperCase;
+    //function to generate password
+    function generatePass(passLength, useSpec, useNum, uppChar) {
 
-console.log(incChar);
-//function to generate password
-function generatePass(passLength) {
+    //assign character variables
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numList = "0123456789";
+    var specChar = "!@#$?+";
+    var passWord = "";
+
+    //variable to include all the selected types
+    var incChar =
+        lowerCase +
+        (useSpec ? specChar : null) +
+        (useNum ? numList : null) +
+        (uppChar ? upperCase : null);
+
+    console.log(incChar);
+
     for (var i = 0; i < passLength; i++) {
-        console.log(Math.ceil(Math.random() * passLength))
-
-    // passWord = Math.floor(Math.random() * passLength.length);
-        //+=
-        //stringArray
-        // step 1 - generate random number to represent index in "inChar" string
-        // step 2 - add the character at that index to the end of the passWord string
+        passWord += incChar.charAt(
+        Math.floor(Math.random() * incChar.length)
+        );
+        console.log(passWord);
     }
+    return alert("You're password is: " + passWord);
+
+    }
+    //invoke function
+    var pw = generatePass(passLength, useSpec, useNum, uppChar);
     
-    //return the password
-    return passWord;
+
 }
 
-//invoke function
-
-alert(generatePass(passLength));
-
-console.log("work");
+init();
